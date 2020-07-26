7722b406bb0c26387a426d3096fbbfe4efd8a5a6
tushar2407 <tushar.mohan2001@gmail.com>
2020-07-27 01:50:26 +0530

    aboutComponen

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
-
 function About(props) {
 
     const leaders = props.leaders.map((leader) => {
+        console.log(leader);
         return (
-            <p>Leader {leader.name}</p>
+            <div key={leader.id} className="col-12 mt-5">
+                <Media tag="li">
+                    <Media left middle>
+                        <Media object src={leader.image} alt={leader.name}/>
+                    </Media> 
+                    <Media body className="ml-5">
+                        <Media heading>{leader.name}</Media>
+                        <p>{leader.designation}</p>
+                        <p>{leader.description}</p>
+                    </Media>
+                </Media>
+            </div>
+            // <p>Leader {leader.name}</p>
         );
     });
 
